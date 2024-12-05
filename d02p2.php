dd8e4f5b53a68314ff846b178a1fbacdfbf46381
Stephane Berube <sberube@devalto.com>
2024-12-05 11:27:19 -0500

    wip: d02p2

--- d02p2.php
+++ d02p2.php
@@ -8,8 +8,9 @@
  * 
  * - The levels are either all increasing or all decreasing.
  * - Any two adjacent levels differ by at least one and at most three.
+ * - Tolerance: 1
  * 
- * @link https://adventofcode.com/2024/day/2
+ * @link https://adventofcode.com/2024/day/2#part2
  */
 
 // Get the Data
@@ -20,9 +21,40 @@ $lines = explode("\n", $data);
 $nbSafe = 0;
 foreach ($lines as $line) {
   $numbers = preg_split('/\s+/', trim($line));
-  if (count($numbers) < 2) {
+  if (count($numbers) <= 2) {
     continue;
   }
+
+  // Cleanup
+  $nbDiffs = count($numbers) - 1;
+  for ($i = 0; $i < $nbDiffs; $i++) {
+    // Remove same level
+    if ((int) $numbers[$i] == (int) $numbers[$i + 1]) {
+      unset($numbers[$i + 1]);
+      break;
+    }
+
+    // Remove high difference
+    $diff = abs((int) $numbers[$i] - (int) $numbers[$i + 1]);
+    if ($diff > 3) {
+      unset($numbers[$i + 1]);
+      break;
+    }
+
+    // Remove level that prevent increasing or decreasing
+    if ($i > 0) {
+      // Current, previous, and next comparison
+      if (
+        ((int) $numbers[$i - 1] < (int) $numbers[$i] && (int) $numbers[$i] > (int) $numbers[$i + 1]) ||
+        ((int) $numbers[$i - 1] > (int) $numbers[$i] && (int) $numbers[$i] < (int) $numbers[$i + 1])
+      ) {
+        unset($numbers[$i]);
+        break;
+      }
+    }
+  }
+
+  $numbers = array_values($numbers); // regenerate the index
 
   // Count
   $nbIncreasing = 0;
